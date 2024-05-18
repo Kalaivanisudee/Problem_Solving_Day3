153a605cf288b32603f834349da94e9e475cd02d
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-18 19:17:42 +0530

    Reversed string basic

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+let string1="Hello World"
+let reversedString = " ";
+for(let i=string1.length-1; i >= 0; i--){
+    reversedString += string1[i];
+}
+console.log(reversedString);// dlroW olleH
+console.log(string1);//Hello World
