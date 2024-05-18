86cd8b4e0329041d8520606e10f924b97e4dbf97
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-18 10:17:18 +0530

    Recursive : Iterative & Recursive approach added

---
+++ index.js
@@ -0,0 +1,18 @@
+//What is recursive function?
+/*A function that calls itself again and again untill a specific condition meets*/
+
+//Iterative Approach
+//1.print 1 to 10
+
+// for(let i=1;i<=10;i++){
+//     console.log(i)//1 2 3 4 5 6 7 8 9 10
+// }
+
+//Recursive Approach
+function printTill(num,current){
+    if(num<current) return;
+    console.log(current)//1 2 3 4 5 6 7 8 9 10
+    printTill(num,current+1)
+
+}
+printTill(10,1)
